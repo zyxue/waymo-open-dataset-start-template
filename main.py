a2c1fd0fdd70767a6033fedd83d87d12367ba609
Zhuyi Xue <zhuyi.xue@alum.utoronto.ca>
2026-01-31 20:51:00 +0000

    added script for checking data access

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import tensorflow as tf
+from waymo_open_dataset.protos import end_to_end_driving_data_pb2 as wod_e2ed_pb2
+
+
 def main():
-    print("Hello from waymo!")
+    for datum in tf.data.TFRecordDataset(
+        tf.io.matching_files(
+            "gs://waymo_open_dataset_end_to_end_camera_v_1_0_0/train*.tfrecord*"
+        ),
+        compression_type="",
+    ).as_numpy_iterator():
+        frame = wod_e2ed_pb2.E2EDFrame()
+        frame.ParseFromString(datum)
+        print(frame)
+        break
 
 
 if __name__ == "__main__":
